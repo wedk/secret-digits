4cb7744a0b9905fdb28a895ae4e5e4cb21df258f
wedk <wedk@users.noreply.github.com>
2019-02-27 16:33:27 +0100

    Updates

---
+++ precache-manifest.415cb1e5763317b5a76722b18c25427e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbaf21e14b4789d35cdc",
+    "url": "/secret-digits/static/css/main.f6141ea6.chunk.css"
+  },
+  {
+    "revision": "fbaf21e14b4789d35cdc",
+    "url": "/secret-digits/static/js/main.fbaf21e1.chunk.js"
+  },
+  {
+    "revision": "b916e5be593541c1677e",
+    "url": "/secret-digits/static/js/runtime~main.b916e5be.js"
+  },
+  {
+    "revision": "e8b73ec2c6aec493189e",
+    "url": "/secret-digits/static/js/2.e8b73ec2.chunk.js"
+  },
+  {
+    "revision": "e368e4d0e191d825442c426c2e95e0b8",
+    "url": "/secret-digits/index.html"
+  }
+];
