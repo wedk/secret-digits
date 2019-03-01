7758de3127c339c076c24d209ed29cf337d225ec
wedk <wedk@users.noreply.github.com>
2019-03-01 07:21:31 +0100

    autofocus the restart button in the success panel

--- src/components/SuccessPanel.js
+++ src/components/SuccessPanel.js
@@ -3,10 +3,18 @@ import React from 'react';
 import styles from './SuccessPanel.module.css';
 
 export default function SuccessPanel({ secretDigits, nbAttempts, onRestart }) {
+  const btnRef = React.useRef();
+
+  // autofocus the restart button on initial render
+  React.useEffect(() => {
+    btnRef.current.focus();
+  }, []);
+
   return (
     <article>
       <div className={styles.commands}>
         <button
+          ref={btnRef}
           type="button"
           onClick={onRestart}
           className={styles.button}
